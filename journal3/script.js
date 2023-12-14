9345d4e880d01bcb8e283ea82af27aef6603f93a
chaisej <143722461+chaisej@users.noreply.github.com>
2023-12-14 05:41:23 -0500

    Add files via upload

---
+++ journal3/script.js
@@ -0,0 +1,22 @@
+$(document).ready(function() {
+
+    $(".question1").click(function(){
+        $(".answer1").toggle();
+    });
+
+    $(".question2").click(function(){
+        $(".answer2").toggle();
+    });
+
+    $(".question3").click(function(){
+        $(".answer3").toggle();
+    });
+
+    $(".question4").click(function(){
+        $(".answer4").toggle();
+    });
+
+    $(".question5").click(function(){
+        $(".answer5").toggle();
+    });
+});
